9e5c4a5eb57c81610dbd3123f4ac65cc98916f04
saisrinivas <vasa.saisrinivas839@gmail.com>
2020-02-04 11:54:07 +0530

    updating index.jsp and adding one java class file

---
+++ HelloWorld_child/src/main/java/com/howtodoinjava/CurrentDateTime.java
@@ -0,0 +1,13 @@
+package com.howtodoinjava;
+
+import java.text.SimpleDateFormat;  
+import java.util.Date;  
+public class CurrentDateTime
+ {  
+public static void main(String[] args)
+ {  
+    SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");  
+    Date date = new Date();  
+    System.out.println(formatter.format(date));  
+}  
+}  
--- HelloWorld_child_war/src/main/webapp/index.jsp
+++ HelloWorld_child_war/src/main/webapp/index.jsp
@@ -1,5 +1,6 @@
 <html>
 <body>
-<h2>Hello World!</h2>
+<h2>Hello World!</h2></br>
+<h1>click for getting currnet date</h1> <button type="submit">Date</button>
 </body>
 </html>
